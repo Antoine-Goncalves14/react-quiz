d2956596dc41f08e28133d9831165703038e86d4
GONCALVES Antoine <antoine.goncalves@audemarspiguet.com>
2026-07-01 11:03:33 +0200

    useState hook

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Question from './Question';
 
 const Quiz = () => {
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+	const testClick = () => {
+		setCurrentQuestionIndex(currentQuestionIndex + 1);
+	};
+
 	return (
 		<div className='quiz'>
 			<div>
 				<div className='score'>Question 1/8</div>
 				<Question />
-				<div className='next-button'>NEXT QUESTION</div>
+				<div className='next-button' onClick={testClick}>
+					NEXT QUESTION {currentQuestionIndex}
+				</div>
 			</div>
 		</div>
 	);
